87154690de772c9de5c889d225964e874b8968eb
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-05-08 12:55:38 -0300

    :bug: fix: user hour format

--- js/modules/displayGreetingMessage.js
+++ js/modules/displayGreetingMessage.js
@@ -2,11 +2,10 @@ export function createGreetingMessage() {
   const greetingsSpan = document.querySelector('[data-greetings]');
   if (greetingsSpan) {
     const now = new Date();
-    const userHour = now.toLocaleTimeString().slice(0, 1);
+    const userHour = now.toLocaleTimeString().slice(0, 2);
     
     const isMorning = userHour >= 6 && userHour < 12;
     const isAfternoon = userHour >= 12 && userHour < 18;
-  
     if (isMorning) {
       greetingsSpan.innerText = 'Good Morning ☕'
     } else if (isAfternoon) {
