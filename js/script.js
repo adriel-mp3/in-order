82b84133952fde1d731100eee69b45146b16163c
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-05-09 12:10:32 -0300

    :sparkles: feat: add drag and drop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { validateKanbanTitleInput } from "./modules/kanbanTitleValidation.js";
 import { createGreetingMessage } from "./modules/displayGreetingMessage.js";
+import { kanbanDragAndDrop } from "./modules/kanbanDragNDrop.js";
 
 validateKanbanTitleInput();
 createGreetingMessage();
+kanbanDragAndDrop();
