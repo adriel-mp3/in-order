e7459de33253dc5e0c9f61c29dfdffc2d73f410a
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-05-13 16:01:54 -0300

    :fire: remove: decoupled database responsibility of tracking active zone

--- js/modules/cards-data.js
+++ js/modules/cards-data.js
@@ -1,42 +1,37 @@
-export const newCardData = {
-  index: 0,
-  date: `0000/00/00`,
-  title: "",
-  priorityClass: "",
-  priorityName: "",
-};
+// export const newCardData = {
+//   title: "",
+//   date: `0000/00/00`,
+//   priorityClass: "",
+//   priorityName: "",
+// };
 
 export const kanbanData = {
   toDoCards: [
     {
-      index: 0,
-      date: `0000/00/00`,
       title: "Card 1",
+      date: `0000/00/00`,
       priorityClass: "high",
       priorityName: "Low",
     },
     {
-      index: 0,
-      date: `0000/00/00`,
       title: "Card 1",
+      date: `0000/00/00`,
       priorityClass: "high",
       priorityName: "Low",
     }
   ],
   pendingCards: [
     {
-      index: 0,
-      date: `0000/00/00`,
       title: "Card 2",
+      date: `0000/00/00`,
       priorityClass: "medium",
       priorityName: "Low",
     },
   ],
   doneCards: [
     {
-      index: 0,
-      date: `0000/00/00`,
       title: "Card 3",
+      date: `0000/00/00`,
       priorityClass: "low",
       priorityName: "Low",
     },
@@ -50,12 +45,12 @@ const year = now.getFullYear();
 const cardTitleElementInput = document.querySelector("#card-name");
 const cardPriorityElementInput = document.querySelector("#card-priority");
 
-export function getNewCardData() {
+export function getModalCardData() {
   newCardData.date = `${year}/${month}/${day}`;
   newCardData.title = cardTitleElementInput.value;
   newCardData.priorityClass = cardPriorityElementInput.value;
   newCardData.priorityName =
-    newCardData.priorityClass.charAt(0).toUpperCase() +
-    newCardData.priorityClass.slice(1);
+  newCardData.priorityClass.charAt(0).toUpperCase() +
+  newCardData.priorityClass.slice(1);
   return newCardData;
 }
