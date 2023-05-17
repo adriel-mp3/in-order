d1d5e02a40273fc3a07255e51bcfe870d5f24b4c
Adriel Santos <109179771+adriel-mp3@users.noreply.github.com>
2023-05-17 13:12:09 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import { createGreetingMessage } from "./modules/display-greetings.js";
 import { validateKanbanTitleInput } from "./modules/kanban-inputs-validation.js";
-import { prepareBoard } from "./modules/cards-functions.js";
 import { kanbanData } from "./modules/cards-data.js";
+import { prepareBoard } from "./modules/cards-functions.js";
 
 const localStorageKanbanData = localStorage.getItem('kanbans');
 
